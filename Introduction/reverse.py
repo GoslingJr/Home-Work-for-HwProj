3b8c2d0370397236849de176a7b0f5ac3577827a
Запорожец глеб <148813894+GoslingJr@users.noreply.github.com>
2025-12-28 15:20:08 +0300

    Delete Introduction directory

--- Introduction/reverse.py
+++
@@ -1,8 +0,0 @@
-def reverse_lexicographic_compare(s1, s2):
-    return s1[::-1] < s2[::-1]
-
-s1 = input("Enter first string: ")
-s2 = input("Enter second string: ")
-
-result = reverse_lexicographic_compare(s1, s2)
-print(f"'{s1}' < '{s2}'" if result else f"'{s1}' >= '{s2}'")
